66879578f134cc746e984398450672222f018921
connor <hiccup08230125@gmail.com>
2024-09-10 01:22:40 -0700

    update assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { PropsWithChildren, Suspense } from 'react';
 import { getURL } from '@/utils/helpers';
 import 'styles/main.css';
 
-const title = 'Next.js Subscription Starter';
-const description = 'Brought to you by Vercel, Stripe, and Supabase.';
+const title = 'Mortgage Rate Update Auto Email Sender';
+const description = 'This helps to update mortgage rates and send email to users.';
 
 export const metadata: Metadata = {
   metadataBase: new URL(getURL()),
